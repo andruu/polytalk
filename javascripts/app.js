738adecc6dd143aeae6ca7174b3cc2cd9023e3ca
andruu <andru.weir@gmail.com>
2012-11-19 19:55:10 -0500

    Added API

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,7 @@ var Polytalk = angular.module('Polytalk', []);
 Polytalk.config(function ($routeProvider) {
   $routeProvider.
     when('/', {templateUrl: 'partials/overview.html'}).
+    when('/api/:language', {templateUrl: 'partials/api.html'}).
     when('/ruby', {templateUrl: 'partials/ruby.html'}).
     when('/php', {templateUrl: 'partials/php.html'}).
     when('/node', {templateUrl: 'partials/node.html'}).
@@ -14,9 +15,30 @@ function AppController ($scope, $location, $rootScope) {
 
   $scope.$on("$routeChangeSuccess", function (event, next, current) {
     $scope.page = $location.path().split('/')[1];
+    if ($scope.page == 'api') {
+      $scope.api = $location.path().split('/')[2];
+      switch ($scope.api) {
+        case 'ruby':
+          $scope.api_name = 'Ruby';
+          break;
+        case 'php':
+          $scope.api_name = 'PHP';
+          break;
+        case 'javascript':
+          $scope.api_name = 'JavaScript';
+          break;
+        case 'coffeescript':
+          $scope.api_name = 'CoffeeScript';
+          break;
+      }
+    } else {
+      $scope.api = null;
+    }
   });
 
   $scope.$on('$viewContentLoaded', function() {
+    $('.method').hide();
+    $('.method.' + $scope.api).show();
     prettyPrint();
   });
     
